2649a5e8242bcfadbdec93cd4a1bb89aac03b757
Patrick West <paddy12309@icloud.com>
2018-02-06 11:53:48 +0000

    Add files via upload

---
+++ session1/scripts/main.js
@@ -0,0 +1,10 @@
+// setup new date and time
+
+var today = new Date();
+//extract hour, minute, second to variables
+var curr_hour = today.getHours();
+var curr_min = today.getMinutes();
+var curr_sec = today.getSeconds();
+
+curr_time = curr_hour+curr_min+curr_sec
+console.log(current_time);
---
+++ session1/scripts/scripts.js
@@ -0,0 +1,2 @@
+    <input type="text" placeholder="name" id="name">
+    <input type="number" placeholder="age" id="age">
